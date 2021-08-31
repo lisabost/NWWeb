6db3958c98a9008c4e659781e4bc6083e347c297
Lisa Bost <bost.lisam@gmail.com>
2021-08-31 15:48:54 -0500

    Toast only shows if no check boxes are checked

--- js/custom.js
+++ js/custom.js
@@ -9,21 +9,21 @@ $(function () {
     //on hover over balloon color change happy birthday's color
     $('.red').hover(function () {
         $('#happy').css('color', 'red');
-    }, function() {
+    }, function () {
         $('#happy').css('color', 'slategray');
     });
-    
+
     $('.green').hover(function () {
         $('#happy').css('color', 'green');
-    }, function() {
+    }, function () {
         $('#happy').css('color', 'slategray');
-    });  
+    });
 
     $('.blue').hover(function () {
         $('#happy').css('color', 'blue');
-    }, function() {
+    }, function () {
         $('#happy').css('color', 'slategray');
-    });  
+    });
 
     // event listener for check/uncheck
     $('.form-check-input').on('change', function () {
@@ -39,13 +39,22 @@ $(function () {
     $('#all').on('click', function (e) {
         e.preventDefault();
         $('.form-check-input').prop('checked', true);
-        //TODO: MAKE BALLOONS APPEAR
-    })
+        $('.form-check-input').each(function () {
+            if ($(this).is(':checked')) {
+                $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown')
+            }
+        });
+    });
+
     $('#none').on('click', function (e) {
         e.preventDefault();
-        $('.form-check-input').prop('checked', false);
-        //TODO: MAKE BALLOONS DISAPPEAR
-    })
+        $('.form-check-input').prop('checked', false)
+        $('.form-check-input').each(function () {
+            if (!$(this).is(':checked')) {
+                $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
+            }
+        });
+    });
 
     var animations = ["animate__animated animate__bounce", "animate__animated animate__flash", "animate__animated animate__pulse", "animate__animated animate__rubberBand",
         "animate__animated animate__shakeX", "animate__animated animate__shakeY", "animate__animated animate__headShake", "animate__animated animate__swing",
@@ -58,9 +67,15 @@ $(function () {
     $('#submit').on('click', function (e) {
         e.preventDefault();
 
-        $('#toast').toast({ autohide: false }).toast('show');
-    });
+        var colors = $("input[name=color]:checked");
 
+        if (colors.length == 0) {
+            $('#toast').toast({ autohide: false }).toast('show');
+        }
+
+
+        
+    });
 
 
 });
